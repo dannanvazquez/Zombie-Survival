67a409ef55b903ee9728eb8a9b91f1f70d50f828
Dannan Vazquez <dannanvazquez@gmail.com>
2022-12-26 03:31:17 -0800

    Ignore other player collisions
    Can walk through other players with the new "Player" layer. Layer also ignored when shooting.

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -36,6 +36,7 @@ public class WeaponController : NetworkBehaviour {
         // Checks if player is looking at an object within maxDistance and set its endPos at the hit object. If there is no object in sight, end bullet at maxDistance.
         Vector3 endPos;
         LayerMask mask = ~LayerMask.GetMask("Ignore Raycast");
+        mask &= ~(1 << 3);
         if (Physics.Raycast(playerController.playerCamera.gameObject.transform.position, playerController.playerCamera.gameObject.transform.TransformDirection(Vector3.forward), out RaycastHit hit, maxDistance, mask)) {
             endPos = hit.point;
             // If the hit object has health, be prepared to deal damage to it.
